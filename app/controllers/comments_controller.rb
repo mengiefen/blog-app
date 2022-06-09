a8049647e17c8c256f80ca36611a5c3c0ec86aeb
mengiefen <mengiefen@gmail.com>
2022-06-09 18:12:08 +0300

    refactor the controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,20 @@
-class CommentsController < ApplicationController
+class CommentsController < PostsController
   def new
     @comment = Comment.new
-    respond_to do |format|
-      format.html { render :new, locals: { comment: @comment } }
-    end
   end
 
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    respond_to do |format|
-      format.html do
-        if @comment.save
-          flash[:success] = 'Comment Created Successfuly!'
-          redirect_to user_post_path(
-            user_id: @post.author_id,
-            id: @post.id
-          )
-        else
-          flash.now[:error] = 'Error: Post could not be saved'
-          # render new
-          render :new, locals: { comment: @comment }
-          # render :new, status: :unprocessable_entity
-        end
-      end
+    # @user = User.find(params[:user_id])
+    # @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to user_post_path(
+        user_id: params[:user_id],
+        id: params[:post_id]
+      )
+    else
+      render :new
     end
   end
 
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,27 @@
 class LikesController < ApplicationController
-  before_action :find_specific_post
+  before_action :find_specific_post, only: [:like]
 
   def new
     @like = Like.new
   end
 
-  def create
+  def like
     return if liked?
+
+    # Like.create(like_params)
+    # redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id]), notice: 'Like Added!'
 
     @post.likes.create(like_params)
     redirect_to user_post_path(
-      user_id: @post.author_id,
-      id: @post.id
+      user_id: params[:user_id],
+      id: params[:post_id]
     )
   end
 
   private
 
   def find_specific_post
-    @post = Post.find_by(id: params[:post_id])
+    @post = Post.find_by(params[:post_id])
   end
 
   def liked?
