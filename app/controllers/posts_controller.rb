8a06b1b259dbc2c4a548dbfb7a59c11477200ea9
mengiefen <mengiefen@gmail.com>
2022-06-10 21:41:14 +0300

    apply review comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,8 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
 
   def index
-    @posts = Post.where(author_id: params[:user_id])
     @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show; end
