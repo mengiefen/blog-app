1a05a7456ce518c8f88100bb14ecaca086b74543
mengiefen <mengiefen@gmail.com>
2022-06-10 10:58:50 +0300

    add validation for post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,10 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 250 }
   validates :text, presence: true
+  validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 
   after_save :increment_post_count
   after_destroy :decrement_post_count
