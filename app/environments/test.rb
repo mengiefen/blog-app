5ea7e735289e89abcba8268826f88b203d2f74e7
mengiefen <mengiefen@gmail.com>
2022-06-13 21:39:12 +0300

    modify package.json

--- app/environments/test.rb
+++ app/environments/test.rb
@@ -1,4 +1,4 @@
-# config.after_initialize do
-#   Bullet.enable = true
-#   Bullet.bullet_logger = true
-# # end
+config.after_initialize do
+  Bullet.enable = true
+  Bullet.bullet_logger = true
+end
